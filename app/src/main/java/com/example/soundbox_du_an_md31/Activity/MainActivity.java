134472fd3fb429818f25aded73c2de0ab72c2457
dquy7 <ddquy772003@gmail.com>
2023-10-13 21:33:22 +0700

    test

--- app/src/main/java/com/example/soundbox_du_an_md31/Activity/MainActivity.java
+++ app/src/main/java/com/example/soundbox_du_an_md31/Activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
