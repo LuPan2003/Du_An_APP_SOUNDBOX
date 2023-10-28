d1ceaa4a7707888615625eab7601fd0faf56ded5
Admin <hieuttph27500@fpt.edu.vn>
2023-10-28 16:19:55 +0700

    Profile

--- app/src/main/java/com/example/soundbox_du_an_md31/Fragment/ChangeInformationFragment.java
+++ app/src/main/java/com/example/soundbox_du_an_md31/Fragment/ChangeInformationFragment.java
@@ -1,20 +1,39 @@
 package com.example.soundbox_du_an_md31.Fragment;
 
+
+
+import android.app.ProgressDialog;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.soundbox_du_an_md31.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 
 public class ChangeInformationFragment extends Fragment {
+    private ProgressDialog progressDialog;
     private ImageView icon_back;
+    private Button btn_updateProfile;
+    private EditText edt_phone, edt_name;
     public static final String TAG = ChangeInformationFragment.class.getName();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,6 +41,14 @@ public class ChangeInformationFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_change_information, container, false);
         icon_back = view.findViewById(R.id.icon_back);
+        edt_name = view.findViewById(R.id.edt_nameUD);
+        edt_phone = view.findViewById(R.id.edt_nunmberphoneUD);
+        btn_updateProfile = view.findViewById(R.id.btn_updateProfile);
+
+        progressDialog = new ProgressDialog(getActivity());
+        setProfile();
+
+
         icon_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,6 +58,54 @@ public class ChangeInformationFragment extends Fragment {
                 }
             }
         });
+        btn_updateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateProfile();
+            }
+        });
         return view;
+    }
+    private void updateProfile(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        progressDialog.show();
+
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(user.getUid(), edt_phone.getText().toString().trim());
+        user.updatePhoneNumber(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getActivity(), "Thêm thành công", Toast.LENGTH_SHORT).show();
+                        } else {
+
+                        }
+                    }
+                });
+
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(edt_name.getText().toString().trim())
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getActivity(), "Cập nhật profile thành công", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void setProfile() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            return;
+        }
+        edt_name.setText(user.getDisplayName());
+        edt_phone.setText(user.getPhoneNumber());
     }
 }
--- app/src/main/java/com/example/soundbox_du_an_md31/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/soundbox_du_an_md31/Fragment/ProfileFragment.java
@@ -1,7 +1,15 @@
 package com.example.soundbox_du_an_md31.Fragment;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -11,17 +19,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.soundbox_du_an_md31.Activity.MainActivity;
 import com.example.soundbox_du_an_md31.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 
 public class ProfileFragment extends Fragment {
+    private ProgressDialog progressDialog;
+    private Uri mUri;
     public static final String TAG = LibraryFragment.class.getName();
-    private ImageView icon_back;
-    private AppCompatButton btn_change_information,btn_change_password;
+    private ImageView icon_back, img_avatarProfile, img_changeIMG;
+    private TextView tv_name, tv_email;
+    private AppCompatButton btn_change_information, btn_change_password, btn_exit;
     private MainActivity mainActivity;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -31,6 +50,26 @@ public class ProfileFragment extends Fragment {
         icon_back = view.findViewById(R.id.icon_back);
         btn_change_information = view.findViewById(R.id.btn_change_infor);
         btn_change_password = view.findViewById(R.id.btn_change_password);
+        img_avatarProfile = view.findViewById(R.id.img_avatarProfile);
+        tv_email = view.findViewById(R.id.tv_email);
+        tv_name = view.findViewById(R.id.tv_name);
+        btn_exit = view.findViewById(R.id.btn_exits);
+        img_changeIMG = view.findViewById(R.id.img_changeImage);
+        progressDialog = new ProgressDialog(mainActivity);
+
+
+        setProfile();
+
+        btn_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+                mainActivity.finish();
+
+            }
+        });
         icon_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,7 +91,89 @@ public class ProfileFragment extends Fragment {
                 mainActivity.gotoChangePassword();
             }
         });
+        initListener();
         return view;
     }
+
+    private void setProfile() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            return;
+        }
+        String name = user.getDisplayName();
+        String email = user.getEmail();
+        Uri photoUrl = user.getPhotoUrl();
+        if (name == null) {
+            tv_name.setVisibility(View.GONE);
+            tv_email.setVisibility(View.GONE);
+
+        } else {
+            tv_name.setVisibility(View.VISIBLE);
+            tv_email.setVisibility(View.VISIBLE);
+            tv_name.setText(name);
+            tv_email.setText(name);
+        }
+
+        Glide.with(getContext()).load(photoUrl).error(R.drawable.avata).into(img_avatarProfile);
+
+
+    }
+
+    private void initListener() {
+        img_changeIMG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickRequestPermission();
+            }
+        });
+    }
+
+    private void onClickRequestPermission() {
+        MainActivity mainActivity = (MainActivity) getActivity();
+        if (mainActivity == null) {
+            return;
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            mainActivity.openGallery();
+            return;
+        }
+        if (getActivity().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            mainActivity.openGallery();
+        } else {
+            String[] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
+            getActivity().requestPermissions(permission, MainActivity.MY_REQUEST_CODE);
+        }
+    }
+
+    public void setImageBitmap(Bitmap imageBitmap) {
+        img_avatarProfile.setImageBitmap(imageBitmap);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user == null){
+            return;
+        }
+        progressDialog.show();
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setPhotoUri(mUri)
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            Toast.makeText(mainActivity, "Cập nhật avatar thành công", Toast.LENGTH_SHORT).show();
+                            setProfile();
+                        }
+                    }
+                });
+    }
+    public void setUri(Uri mUri){
+        this.mUri = mUri;
+    }
+
+
+
 
 }
