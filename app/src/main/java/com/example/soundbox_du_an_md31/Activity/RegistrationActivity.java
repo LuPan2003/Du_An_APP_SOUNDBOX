20bb2106d4ff831a654234d7ff79530ce6e0a1de
LuPan2003 <dungpvph24821@fpt.edu.vn>
2023-11-01 00:15:06 +0700

    APP MUSIC SOUNDBOX lấy id người dùng

--- app/src/main/java/com/example/soundbox_du_an_md31/Activity/RegistrationActivity.java
+++ app/src/main/java/com/example/soundbox_du_an_md31/Activity/RegistrationActivity.java
@@ -19,6 +19,8 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class RegistrationActivity extends AppCompatActivity {
@@ -36,6 +38,7 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
         progressDialog = new ProgressDialog(this);
         mAuth = FirebaseAuth.getInstance();
+
         initUi();
         btnHuy.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,12 +64,27 @@ public class RegistrationActivity extends AppCompatActivity {
     private void createAccount(String email, String password) {
         // [START create_user_with_email]
         progressDialog.show();
+
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         progressDialog.dismiss();
                         if (task.isSuccessful()) {
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            if (user != null) {
+                                String userID = user.getUid();
+                                // Sử dụng ID người dùng ở đây cho mục đích xác thực hoặc quản lý thông tin người dùng.
+                                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+                                // Tạo một node con cho người dùng với ID là userID
+                                DatabaseReference userNode = databaseReference.child("users").child(userID);
+                                userNode.child("email").setValue(user.getEmail());
+                                userNode.child("isVIP").setValue(false);
+                                userNode.child("expiryDate").setValue("2023-12-31");
+                                userNode.child("subscriptionType").setValue("basic");
+                            } else {
+                                // Người dùng chưa đăng nhập
+                            }
                             Toast.makeText(RegistrationActivity.this, "Đăng kí thành công", Toast.LENGTH_SHORT).show();
                             // Sign in success, update UI with the signed-in user's information
                             Intent intent = new Intent(RegistrationActivity.this,LoginActivity.class);
