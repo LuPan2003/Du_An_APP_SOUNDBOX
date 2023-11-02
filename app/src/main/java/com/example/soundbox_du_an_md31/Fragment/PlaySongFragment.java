3a139f3d1a2970f21f4bbebd8a05e1c8736de93e
Admin <hieuttph27500@fpt.edu.vn>
2023-11-02 20:10:07 +0700

    Fontend & Backend danh sách nghe nhạc

--- app/src/main/java/com/example/soundbox_du_an_md31/Fragment/PlaySongFragment.java
+++ app/src/main/java/com/example/soundbox_du_an_md31/Fragment/PlaySongFragment.java
@@ -98,81 +98,41 @@ public class PlaySongFragment extends Fragment implements View.OnClickListener {
         menuMusic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity(), R.style.BottomSheetDialogTheme);
-                View bottomSheetView = LayoutInflater.from(getActivity()).inflate(R.layout.bottomsheet, (LinearLayout) bottomSheetDialog.findViewById(R.id.bottomsheetcontainer));
-                TextView txtaddAlbum = (TextView) bottomSheetView.findViewById(R.id.txt_addAlbum);
-                Song currentSong1 = MusicService.mListSongPlaying.get(MusicService.mSongPosition);
                 FirebaseUser user1 = FirebaseAuth.getInstance().getCurrentUser();
-                DatabaseReference databaseRef1 = FirebaseDatabase.getInstance().getReference();
-                DatabaseReference parentRef1 = databaseRef1.child("album"); // Đường dẫn đến bảng cha
-                DatabaseReference itemRef1 = parentRef1.child(user1.getUid()).child(currentSong1.getId() + "");
-                itemRef1.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if (snapshot.exists()) {
-                            // Phần tử tồn tại
-                            txtaddAlbum.setText("Xóa khỏi danh sách phát");
-                        } else {
-                            // Phần tử không tồn tại
-                            txtaddAlbum.setText("Thêm danh sách phát");
+                if (user1 == null) {
+                    final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity(), R.style.BottomSheetDialogTheme);
+                    View bottomSheetView = LayoutInflater.from(getActivity()).inflate(R.layout.bottomsheet, (LinearLayout) bottomSheetDialog.findViewById(R.id.bottomsheetcontainer));
+                    TextView txtaddAlbum = (TextView) bottomSheetView.findViewById(R.id.txt_addAlbum);
+                    txtaddAlbum.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Toast.makeText(getActivity(), "Chưa đăng nhập tài khoản", Toast.LENGTH_SHORT).show();
+                            
                         }
-                    }
+                    });
+                    bottomSheetDialog.setContentView(bottomSheetView);
+                    bottomSheetDialog.show();
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        // Xử lý khi có lỗi xảy ra
-                    }
-                });
-                txtaddAlbum.setOnClickListener(view1 -> {
-                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                    if (user == null) {
-                        Toast.makeText(getActivity(), "Chưa đăng nhập tài khoản", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                    Song currentSong = MusicService.mListSongPlaying.get(MusicService.mSongPosition);
-                    DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();
-                    DatabaseReference parentRef = databaseRef.child("album"); // Đường dẫn đến bảng cha
-                    DatabaseReference itemRef = parentRef.child(user.getUid()).child(currentSong.getId() + ""); // Đường dẫn đến bảng con và phần tử cần kiểm tra
-                    mFragmentPlaySongBinding.tvSongName.setText(currentSong.getTitle());
-                    itemRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                }
+                else{
+                    final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity(), R.style.BottomSheetDialogTheme);
+                    View bottomSheetView = LayoutInflater.from(getActivity()).inflate(R.layout.bottomsheet, (LinearLayout) bottomSheetDialog.findViewById(R.id.bottomsheetcontainer));
+                    TextView txtaddAlbum = (TextView) bottomSheetView.findViewById(R.id.txt_addAlbum);
+                    Song currentSong1 = MusicService.mListSongPlaying.get(MusicService.mSongPosition);
+
+                    DatabaseReference databaseRef1 = FirebaseDatabase.getInstance().getReference();
+                    DatabaseReference parentRef1 = databaseRef1.child("album"); // Đường dẫn đến bảng cha
+                    DatabaseReference itemRef1 = parentRef1.child(user1.getUid()).child(currentSong1.getId() + "");
+
+                    itemRef1.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
                             if (snapshot.exists()) {
                                 // Phần tử tồn tại
-                                itemRef.removeValue()
-                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                            @Override
-                                            public void onSuccess(Void aVoid) {
-                                                Toast.makeText(getActivity(), "Bỏ thành công", Toast.LENGTH_SHORT).show();
-                                            }
-                                        })
-                                        .addOnFailureListener(new OnFailureListener() {
-                                            @Override
-                                            public void onFailure(@NonNull Exception e) {
-                                                // Xóa thất bại
-                                            }
-                                        });
+                                txtaddAlbum.setText("Xóa khỏi danh sách phát");
                             } else {
                                 // Phần tử không tồn tại
-                                FirebaseDatabase db = FirebaseDatabase.getInstance();
-                                DatabaseReference ref = db.getReference("album/" + user.getUid() + "/" + currentSong.getId());
-//                DatabaseReference parentRef = db.getReference("song/"+ currentSong.getId());
-//                DatabaseReference childRef = parentRef.child("userid");
-//                childRef.setValue(""+user.getUid());
-
-                                Map<String, Object> data = new HashMap<>();
-                                DatabaseReference childRef = ref.child("" + user.getUid());
-                                data.put("artist", currentSong.getArtist());
-                                data.put("count", currentSong.getCount());
-                                data.put("genre", currentSong.getGenre());
-                                data.put("id", currentSong.getId());
-                                data.put("image", currentSong.getImage());
-                                data.put("latest", currentSong.isLatest());
-                                data.put("title", currentSong.getTitle());
-                                data.put("url", currentSong.getUrl());
-                                childRef.setValue(data);
-                                ref.setValue(data);
-                                Toast.makeText(getActivity(), "Thêm thành công", Toast.LENGTH_SHORT).show();
+                                txtaddAlbum.setText("Thêm danh sách phát");
                             }
                         }
 
@@ -181,14 +141,76 @@ public class PlaySongFragment extends Fragment implements View.OnClickListener {
                             // Xử lý khi có lỗi xảy ra
                         }
                     });
+                    txtaddAlbum.setOnClickListener(view1 -> {
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        if (user == null) {
+                            Toast.makeText(getActivity(), "Chưa đăng nhập tài khoản", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        Song currentSong = MusicService.mListSongPlaying.get(MusicService.mSongPosition);
+                        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();
+                        DatabaseReference parentRef = databaseRef.child("album"); // Đường dẫn đến bảng cha
+                        DatabaseReference itemRef = parentRef.child(user.getUid()).child(currentSong.getId() + ""); // Đường dẫn đến bảng con và phần tử cần kiểm tra
+                        mFragmentPlaySongBinding.tvSongName.setText(currentSong.getTitle());
+                        itemRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if (snapshot.exists()) {
+                                    // Phần tử tồn tại
+                                    itemRef.removeValue()
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Toast.makeText(getActivity(), "Bỏ thành công", Toast.LENGTH_SHORT).show();
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    // Xóa thất bại
+                                                }
+                                            });
+                                } else {
+                                    // Phần tử không tồn tại
+                                    FirebaseDatabase db = FirebaseDatabase.getInstance();
+                                    DatabaseReference ref = db.getReference("album/" + user.getUid() + "/" + currentSong.getId());
+//                DatabaseReference parentRef = db.getReference("song/"+ currentSong.getId());
+//                DatabaseReference childRef = parentRef.child("userid");
+//                childRef.setValue(""+user.getUid());
+
+                                    Map<String, Object> data = new HashMap<>();
+                                    DatabaseReference childRef = ref.child("" + user.getUid());
+                                    data.put("artist", currentSong.getArtist());
+                                    data.put("count", currentSong.getCount());
+                                    data.put("genre", currentSong.getGenre());
+                                    data.put("id", currentSong.getId());
+                                    data.put("image", currentSong.getImage());
+                                    data.put("latest", currentSong.isLatest());
+                                    data.put("title", currentSong.getTitle());
+                                    data.put("url", currentSong.getUrl());
+                                    childRef.setValue(data);
+                                    ref.setValue(data);
+                                    Toast.makeText(getActivity(), "Thêm thành công", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                // Xử lý khi có lỗi xảy ra
+                            }
+                        });
 
 
-                    bottomSheetDialog.cancel();
-                });
+                        bottomSheetDialog.cancel();
+                    });
 
 
-                bottomSheetDialog.setContentView(bottomSheetView);
-                bottomSheetDialog.show();
+                    bottomSheetDialog.setContentView(bottomSheetView);
+                    bottomSheetDialog.show();
+                }
+
+
+
 
 
 
