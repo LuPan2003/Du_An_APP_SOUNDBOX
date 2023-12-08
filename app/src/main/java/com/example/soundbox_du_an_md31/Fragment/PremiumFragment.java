b864b392cb8c1b7b75573dc15157b7b41f0e91da
dquy7 <ddquy772003@gmail.com>
2023-12-09 00:50:20 +0700

    fix lỗi zalopay

--- app/src/main/java/com/example/soundbox_du_an_md31/Fragment/PremiumFragment.java
+++ app/src/main/java/com/example/soundbox_du_an_md31/Fragment/PremiumFragment.java
@@ -2,6 +2,7 @@ package com.example.soundbox_du_an_md31.Fragment;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.widget.AppCompatButton;
@@ -14,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.soundbox_du_an_md31.Activity.TestZaloPay;
 import com.example.soundbox_du_an_md31.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -33,7 +35,7 @@ public class PremiumFragment extends Fragment {
 
     public static final String TAG = PremiumFragment.class.getName();
 
-    private AppCompatButton btn_premium1month;
+    private AppCompatButton btnzalopay;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,44 +43,13 @@ public class PremiumFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_premium, container, false);
         icon_back = view.findViewById(R.id.icon_back);
-        btn_premium1month = view.findViewById(R.id.premium_1month);
+        btnzalopay = view.findViewById(R.id.btnZalopay);
 
-        btn_premium1month.setOnClickListener(new View.OnClickListener() {
+        btnzalopay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setTitle("Thông báo");
-                builder.setMessage("Bạn có muốn mua gói premium 1 tháng không?");
-                builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Random random = new Random();
-                        int randomNumber = random.nextInt(10) + 1;
-
-                        if(randomNumber % 2 == 0){
-                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
-                            Map<String, Object> updates = new HashMap<>();
-                            updates.put("isVIP", true);
-                            updates.put("time", 1);
-                            ref.updateChildren(updates);
-                            Toast.makeText(getActivity(), "Thanh toán thành công", Toast.LENGTH_SHORT).show();
-                            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                            if (fragmentManager.getBackStackEntryCount() > 0) {
-                                fragmentManager.popBackStack();
-                            }
-                        }else{
-                            Toast.makeText(getActivity(), "Thanh toán thất bại", Toast.LENGTH_SHORT).show();
-                        }
-
-                    }
-                });
-                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Toast.makeText(getActivity(), "Lỗi", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                Intent intent = new Intent(getActivity(), TestZaloPay.class);
+                startActivity(intent);
             }
         });
 
